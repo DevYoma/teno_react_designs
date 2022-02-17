c4b3488b705bd163f6d2b95f5c713e4a9beb89c7
DevYoma <Lawrenceyoma@gmail.com>
2022-02-17 15:44:52 +0100

    top categories wip

--- src/ANITQUE/components/AntiqueTopCategory/AntiqueTopCategory.jsx
+++ src/ANITQUE/components/AntiqueTopCategory/AntiqueTopCategory.jsx
@@ -4,6 +4,11 @@ import './AntiqueTopCategory.css';
 import LeftCarousel from '../../assets/LeftCarousel.png'
 import RightCarousel from '../../assets/RightCarousel.png'
 import Preview from '../../assets/preview.png';
+import Skull from '../../assets/skull.png';
+import Angel from '../../assets/angel.png';
+import Chair from '../../assets/chair.png';
+import Watch from '../../assets/watch.png';
+import Furniture from '../../assets/furniture.png';
 
 const AntiqueTopCategory = () => {
     return ( 
@@ -18,6 +23,36 @@ const AntiqueTopCategory = () => {
                 </div>
             </div>
 
+            <div className="antiqueTopCategory__flexContainer">
+                <div className="">
+                    <div>
+                        <img src={Preview} alt="Pic1" />
+                    </div>
+                    <div>
+                        <img src={Skull} alt="Pic2" />
+                    </div>
+                </div>
+                <div className="">
+                    <div>
+                        <img src={Angel} alt="Angel" />
+                    </div>
+                </div>
+                <div className="">
+                    <div>
+                        <img src={Chair} alt="Chair" />
+                    </div>
+
+                    <div className="Third__grid row">
+                        <div>
+                            <img src={Watch} alt="Watch" />
+                        </div>
+
+                        <div>
+                            <img src={Furniture} alt="furniture" />
+                        </div>
+                    </div>
+                </div>                
+            </div>
             
         </div>
      );
