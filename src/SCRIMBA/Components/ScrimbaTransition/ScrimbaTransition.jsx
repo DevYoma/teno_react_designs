23a0142e1c5ae3f02cb5f9488495f1ea6dea37bd
DevYoma <Lawrenceyoma@gmail.com>
2022-04-12 12:40:45 +0100

    done with the antique

---
+++ src/SCRIMBA/Components/ScrimbaTransition/ScrimbaTransition.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './ScrimbaTransition.css'
+
+const ScrimbaTransition = () => {
+  return (
+    <div className='scrimbaTransition'>
+        Yoooo
+    </div>
+  )
+}
+
+export default ScrimbaTransition
